f6d443d26c3bd24e9e92e9254632eaec216c5faa
nuclearenergyant <1483773408@qq.com>
2020-05-24 10:27:26 +0800

    2020.5.24.10

--- src/main/java/www/diandian/dao/AddressMapper.java
+++ src/main/java/www/diandian/dao/AddressMapper.java
@@ -1,13 +1,7 @@
 package www.diandian.dao;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
 import www.diandian.entity.Address;
 
-/*对应的是Springboottest0402Application下的@MapperScan*/
-@Mapper
-/*  对应的是TestController下的  @Autowired  */
-@Repository
 public interface AddressMapper {
     int deleteByPrimaryKey(Integer id);
 
